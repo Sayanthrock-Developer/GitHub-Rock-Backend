82b28899a6071baf53a7748497d60e5dbcaba16c
Sayanth Rock <sayanthsmeppayurvaliyaparambil@gmail.com>
2026-07-22 23:04:11 +0530

    fix: import Ktor install for serialization

--- src/main/kotlin/com/sayanthrock/githubrock/backend/plugins/Serialization.kt
+++ src/main/kotlin/com/sayanthrock/githubrock/backend/plugins/Serialization.kt
@@ -2,6 +2,7 @@ package com.sayanthrock.githubrock.backend.plugins
 
 import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
+import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
 import kotlinx.serialization.json.Json
 
