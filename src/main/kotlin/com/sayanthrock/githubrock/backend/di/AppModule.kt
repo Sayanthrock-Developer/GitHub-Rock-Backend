2f566b8d886d392ef836546be0d8ca74a8cbf94b
Sayanth Rock <sayanthsmeppayurvaliyaparambil@gmail.com>
2026-08-25 22:20:23 +0530

    feat(store): register catalog service

--- src/main/kotlin/com/sayanthrock/githubrock/backend/di/AppModule.kt
+++ src/main/kotlin/com/sayanthrock/githubrock/backend/di/AppModule.kt
@@ -6,6 +6,7 @@ import com.sayanthrock.githubrock.backend.security.WebhookVerifier
 import com.sayanthrock.githubrock.backend.service.GitHubDeviceFlowService
 import com.sayanthrock.githubrock.backend.service.GitHubWebOAuthService
 import com.sayanthrock.githubrock.backend.service.HealthService
+import com.sayanthrock.githubrock.backend.service.StoreCatalogService
 import com.sayanthrock.githubrock.backend.storage.WebhookDeliveryRepository
 import com.sayanthrock.githubrock.backend.storage.createDataSource
 import com.zaxxer.hikari.HikariDataSource
@@ -39,4 +40,5 @@ val appModule = module {
     single { HealthService(get(), get<HikariDataSource>(), get(), get()) }
     single { GitHubDeviceFlowService(get(), get()) }
     single { GitHubWebOAuthService(get(), get()) }
+    single { StoreCatalogService(get(), get()) }
 }
